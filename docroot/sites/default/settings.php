c882a381a749ed620f61c709f4c7b2f0b98bcf2f
Peter Drake <peter.drake@acquia.com>
2013-07-17 13:42:52 -0400

    Fixed settings include.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -553,5 +553,5 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
 # $conf['allow_authorize_operations'] = FALSE;
 
 if (file_exists('/var/www/site-php')) {
-  require('/var/www/site-php/d7cloud/d7cloud-settings.inc');
+  require('/var/www/site-php/d7test/d7test-settings.inc');
 }
